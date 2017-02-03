088a5e4c3026d3c3fdccd9de4762b9b559075841
jeffrey <afunpub@gmail.com>
2017-02-03 22:16:59 +0800

    Save my work

---
+++ my_program.py
@@ -0,0 +1,23 @@
+class cat:
+	def __init__(self,name):
+		self.name=name
+	def meow(self):
+		print(self.name+' Meow!')
+	def __str__(self):
+		return 'Cat:一隻叫做'+self.name+'的貓。'
+	'''docstring for cat 
+	def meow(self):
+		print('Meow')
+pusheen = cat()
+pusheen.meow()__init__(self, arg):
+		super(cat_
+		def meow(self):
+			print('Meow')
+	pusheen = cat()
+	pusheen.meow
+()_init__()
+		self.arg = arg
+	'''
+pusheen = cat('Pusheen')
+pusheen.meow()
+print(pusheen)
